d44d2827a98988a6668093ed31726122611ae7f2
Diego Alexandrino <d.s.alexandrino@gmail.com>
2019-04-01 23:08:15 -0300

    Update stack_linked_list.cpp

--- stack_linked_list.cpp
+++ stack_linked_list.cpp
@@ -1,39 +1,48 @@
 # include <new>
-using std::nothrow;
-
+using std::nowthrow;
 template <typename T>
-Struct Stack
+struct Stack
 {
-  Struct Noh
-  {
-    T elem;
-    Noh *prox;
-  };
-  Noh *top;
-  void inicializar()
-  {
-    top = nullptr;
-  }
-  bool empty ()
-  {
-    return top == nullptr;
-  }
-  bool empilhar(T e)
-  {
-    Noh *n = new(nothrow) Noh;
-    if(n == nullptr) { return True; }
-    n -> elem = e;
-    n -> next = top;
-    top = n;
-    return false;
-  }
-  T desempilhar ()
-  {
-    Noh *n = top;
-    top = top -> next;
-    T e = n->elem;
-    delete n;
-    return e;
-  }
-};
+    struct Noh
+    {
+        T elem;
+        Noh *next;
+    };
+    Noh *top;
+    void initialize()
+    {
+        top = nullptr;
+    }
+    bool empty()
+    {
+        return top == nullptr;
+    }
+    bool empilhar(T e)
+    {
+        Noh *n = new(nothrow) Noh;
+        if (n == nullptr)
+        {
+            return True;
+        }
+        n -> elem = e;
+        n -> next = top;
+        top = n;
+        return False;
+    }
+    T desempilhar()
+    {
+        Noh *n = top;
+        top = top -> next;
+        T e = n -> elem;
+        delete n;
+        return e;
+    }
+    void finish()
+    {
+        while(!empty())
+        {
+            desempilhar();
+        }
+    }
+}
     
